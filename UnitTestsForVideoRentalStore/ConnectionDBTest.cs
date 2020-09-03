5c421f16c2a3acb8b9f4af0b451d62f1850818b6
Jotlubana <67535272+Jotlubana@users.noreply.github.com>
2020-09-03 12:18:42 +1200

    Add files via upload

---
+++ UnitTestsForVideoRentalStore/ConnectionDBTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoRentalStore;
+
+namespace UnitTestsForVideoRentalStore
+{
+    [TestClass]
+    public class ConnectionDB
+    {
+        [TestMethod]
+        public void CheckDatabaseConnection_ConnectionOpen_True()
+        {
+            ManageLoginRegister mlr = new ManageLoginRegister();
+            bool assertionValue = mlr.CheckDatabaseConnection();
+            Assert.IsTrue(assertionValue);
+
+        }
+    }
+}
